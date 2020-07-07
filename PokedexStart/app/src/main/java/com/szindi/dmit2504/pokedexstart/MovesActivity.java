abcb6718e3143e211a8eba6e4e4f6b668ccdf32b
Shingai Zindi <szindi@users.noreply.github.com>
2020-07-07 17:38:31 -0600

    Add ListView item layouts

---
+++ PokedexStart/app/src/main/java/com/szindi/dmit2504/pokedexstart/MovesActivity.java
@@ -0,0 +1,13 @@
+package com.szindi.dmit2504.pokedexstart;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MovesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
